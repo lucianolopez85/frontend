a5e64f136888734e5915f3693bc43606f0ea47b0
Luciano <lopezz.lucio85@gmail.com>
2020-11-07 10:03:24 -0300

    add token no header e list no render

--- src/UserLists.js
+++ src/UserLists.js
@@ -2,22 +2,30 @@ import React from 'react'
 import ListComponent from './ListComponent'
 
 export default class UserLists extends React.Component{
-    state = { lists: null, loading: true }
+    state = { lists: [], loading: true }
 
     async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token e128c1db4a4b8b8ec5cc9ad180bc7e476a60aa98 '; 
+
         var url = 'http://127.0.0.1:8000/list/';
-        const response = await fetch(url);
+        const response = await fetch(url, config);
         const data = await response.json();
         console.log(data);
         this.setState({lists: data, loading: false});
     }
+
     render()
     {
+        const listsApi = this.state.lists;
         return (
             <div>
-                <ListComponent listName={'Minha Lista'} />
-                <ListComponent listName={'Minha Lista 2'} />
+                {listsApi.map(list => <ListComponent key={list.id} listName={list.name} />)}
             </div>
         )
-        } 
+    } 
 }
